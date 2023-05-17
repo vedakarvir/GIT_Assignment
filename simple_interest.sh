06bd6a158e1e518c238dd1481b121d4d7e807ee9
vedakarvir <53219050+vedakarvir@users.noreply.github.com>
2023-05-17 19:24:02 +0100

    Create simple_interest.sh
    Created a basic interest calculation script

---
+++ simple_interest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Author : Veda Karvir
+# This script calculates simple interest
+
+echo "Enter loan amount:"
+read AMOUNT
+echo "Enter rate of interest:"
+read INT
+echo "Enter number of years:"
+read YEARS
+echo "$YEARS"
+TOT_INT=$((AMOUNT*INT*YEARS/100))
+TOT_AMOUNT=$((AMOUNT+TOT_INT))
+echo "Total interest to be paid over $YEARS years : $TOT_INT"
+echo "Total amount including interest: $TOT_AMOUNT"
